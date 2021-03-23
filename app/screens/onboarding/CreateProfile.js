185e97a0a0efc16608ad8426ff4cd3419e44cb2f
Trentist <chasegreer327@gmail.com>
2021-03-24 03:08:06 +0500

    Facebook+Google-SignIn

--- app/screens/onboarding/CreateProfile.js
+++ app/screens/onboarding/CreateProfile.js
@@ -1,37 +1,59 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity, TextInput } from 'react-native';
-import { Dropdown } from 'react-native-material-dropdown'
-import { ImageButton, SmallButton } from '../../components/common';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import {Dropdown} from 'react-native-material-dropdown';
+import {ImageButton, SmallButton} from '../../components/common';
 import assets from '../../assets';
 
 const CreateProfile = ({navigation}) => {
-  let data = [{
-    value: 'Toronto, Canada',
-  }, {
-    value: 'Vancouver, Canada',
-  }, {
-    value: 'New York, US',
-  }];
+  let data = [
+    {
+      value: 'Toronto, Canada',
+    },
+    {
+      value: 'Vancouver, Canada',
+    },
+    {
+      value: 'New York, US',
+    },
+  ];
   return (
     <View style={styles.container}>
-
-      <ImageButton onPress={()=>navigation.goBack()} style={styles.backButton} source={assets.images.icons.back} />
+      <ImageButton
+        onPress={() => navigation.goBack()}
+        style={styles.backButton}
+        source={assets.images.icons.back}
+      />
 
       <Text style={styles.title}>Create Profile</Text>
       <View style={styles.avatarCont}>
         <TouchableOpacity style={styles.touchable}>
-          <Image style={{width: 45, height: 40}} source={assets.images.icons.uploadIcon} />
+          <Image
+            style={{width: 45, height: 40}}
+            source={assets.images.icons.uploadIcon}
+          />
         </TouchableOpacity>
       </View>
       <Text style={styles.description}>Choose your Avatar</Text>
       <TextInput style={[styles.textInput, {height: 50}]} placeholder="Name" />
-      <Dropdown
-        label='Location'
-        data={data}
-        containerStyle={styles.dropdown}
+      <Dropdown label="Location" data={data} containerStyle={styles.dropdown} />
+      <TextInput
+        style={[styles.textInput, {height: 120}]}
+        placeholder="About"
+        multiline
+        textAlignVertical="top"
       />
-      <TextInput style={[styles.textInput, {height: 120}]} placeholder="About" multiline textAlignVertical="top" />
-      <SmallButton onPress={()=>navigation.navigate('Main')} style={{width: 100, marginTop: 20}} title="Done"/>
+      <SmallButton
+        onPress={() => navigation.navigate('Main')}
+        style={{width: 100, marginTop: 20}}
+        title="Done"
+      />
     </View>
   );
 };
@@ -44,37 +66,37 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   backButton: {
     alignSelf: 'flex-start',
     marginTop: 20,
-    marginBottom: 20
+    marginBottom: 20,
   },
   title: {
     alignSelf: 'flex-start',
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 30
+    marginBottom: 30,
   },
   avatarCont: {
     width: 140,
     height: 140,
     borderWidth: 1,
     borderColor: '#707070',
-    borderRadius: 70
+    borderRadius: 70,
   },
   touchable: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   description: {
     fontSize: 16,
     color: '#B1B1B1',
-    marginTop: 15
+    marginTop: 15,
   },
   textInput: {
     borderColor: '#707070',
@@ -96,8 +118,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     paddingHorizontal: 18,
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
 });
 
 export default CreateProfile;
--- app/screens/onboarding/Login.js
+++ app/screens/onboarding/Login.js
@@ -1,8 +1,121 @@
-import React from 'react';
-import {View, StyleSheet, Text, TextInput} from 'react-native';
+import React,{useState,useEffect} from 'react';
+import {View, StyleSheet, Text, TextInput,Button} from 'react-native';
 import {LargeButton, TextLink, SocialButton} from '../../components/common';
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import {
+  GoogleSignin,
+  statusCodes,
+} from 'react-native-google-signin';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
+  
 
 const Login = ({navigation}) => {
+  const [loggedIn, setloggedIn] = useState(false);
+  const [user, setUser] = useState([]);
+
+
+  //Google SignIn Method
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {accessToken, idToken} = await GoogleSignin.signIn();
+      setloggedIn(true);
+
+      const credential = auth.GoogleAuthProvider.credential(
+        idToken,
+        accessToken,
+      );
+      await auth().signInWithCredential(credential);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        alert('Cancel');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('Signin in progress');
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('PLAY_SERVICES_NOT_AVAILABLE');
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+  function onAuthStateChanged(user) {
+    setUser(user);
+    console.log(user);
+    
+      if(user)
+      {
+        setloggedIn(true);
+        navigation.navigate('Main')
+      }
+      
+      
+      
+
+  
+  }
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId:
+        '583315892753-k3r3uqvo2gepl8sf89me5dkk32cldhn7.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    });
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  //Google Signout
+  const signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      auth()
+        .signOut()
+        .then(() => alert('Your are signed out!'));
+      setloggedIn(false);
+      // setuserInfo([]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  //FaceBook Login
+
+  async function FacebookLogin() {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+  
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+  
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+  
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+  
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
+  }
+  
+   function logout()
+   {
+    
+    LoginManager.logOut()
+    Alert.alert('logout')
+   }
+
+
   return (
     <View style={styles.container}>
       <TextInput style={styles.textInput} placeholder="Email Address" />
@@ -20,9 +133,18 @@ const Login = ({navigation}) => {
       </View>
       <Text style={styles.loginWith}>or login with</Text>
       <View style={styles.socialCont}>
-        <SocialButton style={styles.socialButton} social="google" />
-        <SocialButton style={styles.socialButton} social="facebook" />
+        <SocialButton onPress={signIn} style={styles.socialButton} social="google" />
+        <SocialButton onPress={FacebookLogin} style={styles.socialButton} social="facebook" />
         <SocialButton style={styles.socialButton} social="twitter" />
+                     <Button
+                    onPress={signOut}
+                    title="LogOut"
+                    color="red"></Button>
+
+                    <Button
+                    onPress={logout}
+                    title="LogOut"
+                    color="blue"></Button>
       </View>
       <View style={styles.bottomContainer}>
         <Text style={styles.bottomText}>Don't have an account? </Text>
